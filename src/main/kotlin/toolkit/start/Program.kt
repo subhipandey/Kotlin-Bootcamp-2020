d9e01c469a8e2dc95f2d29652b5df4d7505d9229
Subhi Pandey <subhi2012cpi@gmail.com>
2020-08-16 17:21:57 +0530

    solution for map and ranges

--- src/main/kotlin/toolkit/start/Program.kt
+++ src/main/kotlin/toolkit/start/Program.kt
@@ -6,6 +6,30 @@ import java.text.DecimalFormat
 fun main() {
     val formatter = DecimalFormat(".00")
     val input = (1..10).map { (1..10).map { random() } }
-    //Print out all the numbers which are greater that 0.5
-    // formatted to two decimal places via 'formatter'
+
+    solution1(input, formatter)
+    solution2(input, formatter)
+    solution3(input, formatter)
+}
+
+private fun solution1(input: List<List<Double>>, formatter: DecimalFormat) {
+    println("Solution no 1")
+    input.flatMap { it }
+        .filter { num -> num > 0.5 }
+        .map { num -> formatter.format(num) }
+        .forEach(::println)
+}
+
+private fun solution2(input: List<List<Double>>, formatter: DecimalFormat) {
+    println("Solution no 2")
+    input.flatMap { it.filter { num -> num > 0.5 } }
+        .map { num -> formatter.format(num) }
+        .forEach(::println)
+}
+
+private fun solution3(input: List<List<Double>>, formatter: DecimalFormat) {
+    println("Solution no 3")
+    input.flatMap { it.filter { num -> num > 0.5 }
+        .map { num -> formatter.format(num) } }
+        .forEach(::println)
 }
